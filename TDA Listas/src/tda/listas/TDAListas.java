86ffec02ee9560e87b819f9bcc3bacda79076b0e
sebastianr2003 <sebasrickytapia@gmail.com>
2025-10-22 00:02:54 -0400

    parcial 2

--- TDA Listas/src/tda/listas/TDAListas.java
+++ TDA Listas/src/tda/listas/TDAListas.java
@@ -2,27 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package tdamatriz;
+package tda.listas;
 
 /**
  *
- * @author sebas
- * git add . todo la carpeta
- * git commit -m ""
- * git push sube
- * git pull baja
+ * @author Rocka
  */
-public class TDAMatriz {
+public class TDAListas {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Sparce m= new Sparce(3,3,3); 
-        m.setdato(1,1,62);
-        m.mostrarvector();
-        System.out.println(m);
     }
     
 }
