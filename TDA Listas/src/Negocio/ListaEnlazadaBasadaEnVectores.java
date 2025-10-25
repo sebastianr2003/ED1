e378f9344cb75bf4545dee8b6b316b9c83095a42
sebastianr2003 <sebasrickytapia@gmail.com>
2025-10-24 20:48:04 -0400

    lebev listo

--- TDA Listas/src/Negocio/ListaEnlazadaBasadaEnVectores.java
+++ TDA Listas/src/Negocio/ListaEnlazadaBasadaEnVectores.java
@@ -8,26 +8,35 @@ import java.util.ArrayList;
  *
  * @author sebas
  */
-class Nodos1{
+class Nodos1{//atributos de nodos1
     int valor;//VALOR
     int siguiente;//SIGUIENTE DIRRECCION
-    public Nodos1(int valor,int siguiente){//MANDAMOS VALOR Y SIGUIENTE
-        this.valor=valor;
+    public Nodos1(int valor,int siguiente){//constructor de nodos1
+        this.valor=valor;//asigna a atributos de la clase
         this.siguiente=siguiente;
     }
 }
     
-public class ListaEnlazadaBasadaEnVectores {//lista de nodos
-    private ArrayList<Nodos1>nodos;//nueva arraylist atributo ndod
+public class ListaEnlazadaBasadaEnVectores {//lista de nodos atributos
+    private ArrayList<Nodos1> nodos;//nueva arraylist atributo ndod
     private int primerelemento;//primer elemento
     private int ultimoelemento;//ultimo elemento
-    public ListaEnlazadaBasadaEnVectores (){//constructor
+    public  ListaEnlazadaBasadaEnVectores (){//constructor de lista de  nodos
         nodos=new ArrayList<>();//nodo inical 
         nodos.add(new Nodos1(0,-1));//es como el P
         primerelemento=-1;
         ultimoelemento=-1;        
     } 
-    publi void agregarelemento(int valor){
+    public void agregarelemento(int valor){
+        int nuevoindice=nodos.size();//inicia en posicion+ a la actual no hay pos=0 1234
+        nodos.add(new Nodos1(valor,-1));//P-> creado
+        if(ultimoelemento==-1){//si no hay ningun elemento
+            primerelemento=nuevoindice;
+            ultimoelemento=nuevoindice;
+            return;
+        } 
+        nodos.get(ultimoelemento).siguiente=nuevoindice;// nodo del ultimo elemento  y el siguiente nodo es nuevo indice
+        ultimoelemento=nuevoindice;//el ultimo nodo es el nuevo indice
         
     }
 }
